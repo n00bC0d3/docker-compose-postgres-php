63f849d6a0b1b6e8a4eb5b996537e16ddb38d4d7
n00bC0d3 <kurniawan.moonlight@gmail.com>
2023-04-12 18:57:44 +0700

    first commit

---
+++ webapp/index.php
@@ -0,0 +1,9 @@
+<?php
+$db_connection = pg_connect("host=postgres_host dbname=webapp_db user=nano");
+if ($db_connection) {
+    echo " koneksi ke Service DB PGSQL berhasil"
+}
+else {
+    echo "gagal konek!!"
+}
+?>
