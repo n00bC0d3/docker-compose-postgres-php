c808df02c11d20b1b2d5c196e1026ef3774331d1
n00bC0d3 <kurniawan.moonlight@gmail.com>
2023-04-12 19:26:59 +0700

    fix php syntax

--- webapp/index.php
+++ webapp/index.php
@@ -1,9 +1,8 @@
 <?php
 $db_connection = pg_connect("host=postgres_host dbname=webapp_db user=nano");
 if ($db_connection) {
-    echo " koneksi ke Service DB PGSQL berhasil"
-}
-else {
-    echo "gagal konek!!"
+    echo " koneksi ke Service DB PGSQL berhasil";
+} else {
+    echo "gagal konek!!";
 }
 ?>
